04e4418886cbec60a1e7b472c6b8290a56b4c0ae
David Cardoso <davidcardoso93@gmail.com>
2017-09-04 11:49:45 -0400

    Nested file serializers within Snippet response

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -3,21 +3,23 @@ from rest_framework import serializers
 from .models import Group, Snippet, File
 
 # Serializer to create representations of data into JSON. Using ModelSerializer for ease of use
+class FileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = File
+        fields = ('id', 'snippet_id', 'title', 'description', 'language', 'code')
+
 class SnippetSerializer(serializers.ModelSerializer):
+    files = FileSerializer(many=True, read_only=True)
+
     class Meta:
         model = Snippet
         files = serializers.StringRelatedField(many=True) # StringRelated outputs the whole string, instead of just the ID
         fields = ('id', 'title', 'description', 'files')
         read_only_fields = ('created', 'modified')
-        
+
 class GroupSerializer(serializers.ModelSerializer):
     snippets = SnippetSerializer(many=True, read_only=True)  # StringRelated outputs the whole string, instead of just the ID
 
     class Meta:
         model = (Group)
         fields = ('id', 'title', 'label_color', 'snippets')
-
-class FileSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = File
-        fields = ('id', 'snippet_id', 'title', 'description', 'language', 'code')
