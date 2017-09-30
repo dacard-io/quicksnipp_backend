b09ace8b33672dd85a77a39acd1779f32fbf77d6
David Cardoso <davidcardoso93@gmail.com>
2017-09-30 01:07:54 -0400

    Slight change to snippet serializer to display group_id

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -14,7 +14,7 @@ class SnippetSerializer(serializers.ModelSerializer):
     class Meta:
         model = Snippet
         files = serializers.StringRelatedField(many=True) # StringRelated outputs the whole string, instead of just the ID
-        fields = ('id', 'title', 'description', 'files')
+        fields = ('id', 'group_id', 'title', 'description', 'files')
         read_only_fields = ('created', 'modified')
 
 class GroupSerializer(serializers.ModelSerializer):
